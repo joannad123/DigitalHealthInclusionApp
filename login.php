77a395d3851163c139787fee133b43a4a4ed8cf3
joannad123 <36174385+joannad123@users.noreply.github.com>
2019-01-08 10:34:56 +0000

    Information slideshow added

---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<h1>Login/Register</h1>
+<div class = "login">
+
+    <form id= "login" method="post">
+
+    <p>Username</p>
+    <input type = "text" id="username">
+    <p>Email Address</p>
+    <input type = "email" id="email">
+    <p>Password</p>
+    <input type = "password" id="password">
+
+    <p><input type = "submit" id="submit"></p>
+</div>
+
+</body>
+</html>
