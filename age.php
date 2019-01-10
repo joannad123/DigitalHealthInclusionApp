5c3709e93728eb2dd654577f1106a56da90c6a68
joannad123 <36174385+joannad123@users.noreply.github.com>
2019-01-10 17:09:22 +0000

    Login functionality began
    Can insert username and password into database. Can update age but need to get it to update logged in user.

--- age.php
+++ age.php
@@ -7,18 +7,43 @@
 </head>
 <body>
 <h1>What age are you?</h1>
+
+<form name = "login" method="post">
+
 <div class = "age">
 
-    <form id= "age" method="post">
-
         <p>Age</p>
-        <input type = "number" id="username">
+        <input type = "number" name="age">
 
-        <p><input type = "submit" id="submit"></p>
-
-        <p><input type = "button" value="Next" id="next" onclick="window.location = 'ability.php'"</p>
+        <p><input type = "submit" name="submit"></p>
 
 </div>
 
+    <?php
+
+    $age = strip_tags((isset($_POST["age"]) && is_numeric($_POST["age"])) ? $_POST["age"] : "");
+
+    $host = "devweb2018.cis.strath.ac.uk";
+    $user = "wjb15159";
+    $pass = "vahm0Iez4eex";
+    $dbname = "wjb15159";
+    $conn = new mysqli($host, $user, $pass, $dbname);
+
+    if($conn->connect_error) {
+        die("Connection failed".$conn->connect_error);
+    }
+
+
+    //TODO : check username that is logged in and update age for that entry
+
+    $sql = "UPDATE user_score SET age='$age' WHERE username='joanna'";
+
+    if($conn->query($sql) === TRUE) {
+        echo "<p>You have successfully submitted your age.</p>";
+    }
+
+    ?>
+
+</form>
 </body>
 </html>
--- login.php
+++ login.php
@@ -8,20 +8,70 @@
 <body>
 <h1>Please provide a username or<br>
     login to an existing account</h1>
+
+<form name = "login" method="post">
+
 <div class = "login">
-
-    <form id= "login" method="post">
-
     <p>Username</p>
-    <input type = "text" id="username">
+    <input type = "text" name="username" >
 
     <p>Password</p>
-    <input type = "password" id="password">
+    <input type = "password" name="password" >
 
-    <p><input type = "submit" id="submit""></p>
+    <p><input type = "submit" name="submit"></p>
 
-     <p><input type = "button" value="Next" id="next" onclick="window.location = 'age.php'"</p>
 </div>
 
+    <?php
+
+    session_start();
+
+    $username = strip_tags((isset($_POST["username"]) ? $_POST["username"] : ""));
+    $password = (isset($_POST["password"]) ? md5($_POST["password"]) : "");
+
+    $host = "devweb2018.cis.strath.ac.uk";
+    $user = "wjb15159";
+    $pass = "vahm0Iez4eex";
+    $dbname = "wjb15159";
+    $conn = new mysqli($host, $user, $pass, $dbname);
+
+    if($conn->connect_error) {
+        die("Connection failed".$conn->connect_error);
+    }
+
+    if(($username != "") && checkUsername($conn, $username)){
+        $username = "";
+        $usernameError = "There is already an account associated with that username, please choose another one.";
+        echo "<p id='error'>".$usernameError."</p>";
+    }
+
+    if(isset($_SESSION["username"]) && isset($_SESSION["password"])){
+        $username = $_SESSION["username"];
+        $password = $_SESSION["password"];
+    }
+
+
+    $sql = "INSERT INTO `user_score` (`username`, `password`, `age`, `ability`, `frequency`, `score`) VALUES ('$username', '$password', '12', '1', '2', '3');";
+
+    if($conn->query($sql) === TRUE){
+        header("Location: age.php");
+        exit();
+    }
+
+    function checkUsername($connection, $username)
+    {
+        $sqlUsers = "SELECT * FROM `user_score` WHERE `username` = '$username'";
+        $resultLogin = $connection->query($sqlUsers);
+
+        if ($resultLogin->num_rows > 0) {
+            return true;
+        }
+        return false;
+    }
+
+    ?>
+
+</div>
+</form>
 </body>
 </html>
