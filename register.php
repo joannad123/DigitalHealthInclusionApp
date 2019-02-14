de0b756acaab8319f3b2ac9bdc498224ee1a57a8
joannad123 <36174385+joannad123@users.noreply.github.com>
2019-02-14 15:37:03 +0000

    Added error message on register
    When no fields are filled, alert appears

--- register.php
+++ register.php
@@ -19,7 +19,7 @@
         <p><u>Password</u></p>
         <input type = "password" name="password" >
 
-        <p><input type = "submit" name="submit"></p>
+        <p><input name = "button" type = "submit" value = "Submit"></p>
 
         <p><h2> If you already have an account, click go to login<br>
                 If you'd like a tutorial on logging in/registering, click go to tutorial</h2></p>
@@ -38,23 +38,30 @@
     $username = strip_tags((isset($_POST["username"]) ? $_POST["username"] : ""));
     $password = (isset($_POST["password"]) ? ($_POST["password"]) : "");
 
-    if(($username != "") && checkUsername($conn, $username)){
-        $username = "";
-        echo "<script>
+    if (isset($_POST['button'])) {
+
+        if (($username != "") && checkUsername($conn, $username)) {
+            $username = "";
+            echo "<script>
         alert('There is already an account associated with that username, please choose another one.');
         </script>";
-    }
+        }
 
-    $sql = "";
+        $sql = "";
 
-    if($username != "" && $password != "") {
-        $sql = "INSERT INTO `user_score` (`username`, `password`, `age`, `ability`, `frequency`, `score`, `logincount`, `loginquiz`, `safetyquiz`, `appquiz`, `usability`, `usability2`) VALUES ('$username', '$password', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0');";
-        if ($conn->query($sql) === TRUE) {
-            echo "<script>
+        if ($username != "" && $password != "") {
+            $sql = "INSERT INTO `user_score` (`username`, `password`, `age`, `ability`, `frequency`, `score`, `logincount`, `loginquiz`, `safetyquiz`, `appquiz`, `usability`, `usability2`) VALUES ('$username', '$password', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0');";
+            if ($conn->query($sql) === TRUE) {
+                echo "<script>
         alert('Account created successfully, now login');
         window.location.href='login.php';
         </script>";
-            exit();
+                exit();
+            }
+        } else {
+            echo "<script>
+        alert('Make sure you are entering a username and password');
+        </script>";
         }
     }
 
